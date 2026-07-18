4c61b2273d7c139a6a01c98ad639248c9a568ce9
G <g@devnull.rip>
2026-07-17 17:36:49 -0700

    ex2

--- main.go
+++ main.go
@@ -1,39 +1,51 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 
-	"github.com/g-lok/bootdev-gatorblogs/internal/config"
+	"github.com/g-lok/bootdev-gatorblogs/cmd"
 )
 
 const userName = "g"
 
+// func getConfig() config.Config {
+// 	currentCfg, err := config.Read()
+// 	if err != nil {
+// 		errMsg := fmt.Errorf("failed to read .gatorconfig.json: %w", err)
+// 		fmt.Fprintf(os.Stderr, "error: %v\n", errMsg)
+// 		os.Exit(1)
+// 	}
+//
+// 	return *currentCfg
+// }
+
 func main() {
-	currentCfg, err := config.Read()
+	// currentCfg := getConfig()
+	//
+	// err := currentCfg.SetUser(userName)
+	// if err != nil {
+	// 	errMsg := fmt.Errorf("failed to set UserName: %w", err)
+	// 	fmt.Fprintf(os.Stderr, "error: %v\n", errMsg)
+	// }
+	//
+	// updatedConfig := getConfig()
+	//
+	// cfg, err := json.MarshalIndent(updatedConfig, "", "  ")
+	// if err != nil {
+	// 	errMsg := fmt.Errorf("failed to marshall .gatorconfig: %w", err)
+	// 	fmt.Fprintf(os.Stderr, "error: %v\n", errMsg)
+	// 	os.Exit(1)
+	// }
+	//
+	// fmt.Println(string(cfg))
+	// args := os.Args
+	// if len(args) <= 2 {
+	// 	fmt.Println("error: cli requires at least 2 argument keywords")
+	// }
+	err := cmd.Root()
 	if err != nil {
-		err := fmt.Errorf("failed to read .gatorconfig.json: %w", err)
-		fmt.Fprintf(os.Stderr, "error: %w", err)
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
-
-	err = currentCfg.SetUser(userName)
-	if err != nil {
-		err := fmt.Errorf("failed to set UserName: %w", err)
-		fmt.Fprintf(os.Stderr, "error: %w", err)
-	}
-
-	updatedConfig, err := config.Read()
-	if err != nil {
-		err := fmt.Errorf("failed to read .gatorconfig.json: %w", err)
-		fmt.Fprintf(os.Stderr, "error: %w", err)
-	}
-
-	cfg, err := json.MarshalIndent(updatedConfig, "", "  ")
-	if err != nil {
-		err := fmt.Errorf("failed to marshall .gatorconfig: %w", err)
-		fmt.Fprintf(os.Stderr, "error: %w", err)
-	}
-
-	fmt.Println(string(cfg))
 }
